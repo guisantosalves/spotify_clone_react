b1009f3b7b8f0c3b5641d70cd9e09a0e7269dc03
guisantosalves <workguisantos@gmail.com>
2022-06-09 13:06:27 -0400

    making login page and setting the spotify token using reduce method

---
+++ src/components/Login.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./css/Login.css";
+import { loginUrl, getTokenFromUrl } from "../spotify";
+
+function Login() { 
+
+  const token = getTokenFromUrl();
+
+  console.log(token);
+
+  return (
+    <div className="login">
+      <h1>login page</h1>
+      {/* spotifu Logo */}
+      <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt="spotify logo"
+      />
+
+      {/* Login with spotify button */}
+      <a href={loginUrl}>Login with spotify</a>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/spotify.js
@@ -0,0 +1,35 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/";
+
+const clientID = "1d1b1a79b7ec4e4eacba2b15517a07b0";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+//get everythin in front of #
+export const getTokenFromUrl = () => {
+    return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item)=>{
+        
+        var parts = item.split("=")
+
+        
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+
+        return initial;
+
+    }, {})
+} 
+
+//we need build this URL to catch the token of spotify
+export const loginUrl = `${authEndpoint}?client_id=${clientID}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
